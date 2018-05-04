d6c57597ba9de1822e778f50b542dd1056b03a99
ulvsystems <38928402+ulvsystems@users.noreply.github.com>
2018-05-03 22:59:39 -0300

    Tiberi: ajustes de imagens, carrossel(nao esta pronto)

--- sobre.php
+++ sobre.php
@@ -1,12 +1,35 @@
 <?php include 'nav_bar.php';?>
-<div class="header fundo">
-  <div class="row">
+
+
+<div id="carouselExampleControls" class="carousel slide fundo" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/tiberi/images/imagem-1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/tiberi/images/imagem-2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/tiberi/images/imagem-3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+  <!-- <div class="row">
     <div class="container apresentacao col-sm-12">
       <h1>Tiberi Representações</h1>
       <button type="button" class="btn btn-light">Saiba mais! <i class="far fa-hand-point-down"></i></button>
     </div>
   </div>
-</div>
+</div> -->
 
 
   <div class="middle">
@@ -39,3 +62,5 @@
 			</div>
 		</div>
 	</div>
+
+  <script src="/tiberi/js/sobre.js"></script>
