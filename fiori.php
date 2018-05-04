69256159acffe7c0a72c59e3b534807046827e73
ulvsystems <38928402+ulvsystems@users.noreply.github.com>
2018-05-03 22:59:39 -0300

    Tiberi: ajustes de imagens, carrossel(nao esta pronto)

--- fiori.php
+++ fiori.php
@@ -11,7 +11,16 @@
   <div class="row">
     <div class="col-sm-4">
       <img src="https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a51a539369632befc4981ced1606764a&auto=format&fit=crop&w=750&q=80" alt="" class="img-thumbnail">
-      <p>balbalab</p>
+      <p>
+        <button class="btn btn-link descricao" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Descrição
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
     </div>
     <div class="col-sm-4">
       <img src="https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a51a539369632befc4981ced1606764a&auto=format&fit=crop&w=750&q=80" alt="" class="img-thumbnail">
